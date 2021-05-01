74adffa09edf7921d5cefe4e861e142373b67ce0
Witas1703 <taisner.witold@gmail.com>
2021-05-01 20:48:34 +0200

    implemented shinydashboard

--- ui.R
+++ ui.R
@@ -1,10 +1,24 @@
 library(shiny)
+library(shinydashboard)
 
-shinyUI(navbarPage("London",
-    tabPanel("Viz1",
-             mainPanel(
-                dataTableOutput("animals")
-                )
-             ),
-    tabPanel("Viz2")
-    ))
+dashboardPage(
+  dashboardHeader(title = "London"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Main page", tabName = "1", icon = icon("dashboard")),
+      menuItem("Table", tabName = "2", icon = icon("th")),
+      menuItem("Viz2", tabName = "3", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+  tabItems(
+    tabItem(tabName = "1",
+            h2("trelemorele")),
+    tabItem(tabName = "2", 
+            h2('adam grzenda'),
+            dataTableOutput("animals")),
+    tabItem(tabName = "3", 
+            h1("CZEŚĆ ANIA"))
+    )
+  )
+)
