46d166ef867d9d20e27993b30b53e40ba0303998
Witas1703 <taisner.witold@gmail.com>
2021-05-22 22:25:58 +0200

    no change, I'm retarded :( had to fix my git

--- server.R
+++ server.R
@@ -9,210 +9,106 @@ library(ggridges)
 library(rgdal)
 library(sp)
 library(rgeos)
-<<<<<<< HEAD
-library(shinydashboard)
 library(inlmisc)
-library(shinyWidgets)
-=======
-library(inlmisc)
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
 library(RColorBrewer)
 library(rcartocolor)
 library(reactable)
 library(htmltools)
-<<<<<<< HEAD
-=======
 library(shinyWidgets)
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
-
-# ----------------------------------------- Ania's chad code ---------------------------------------------------------------------
-covid.london_england = read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=region&metric=cumCasesBySpecimenDate&format=csv"))
-covid.london_england = covid.london_england %>% as_tibble() %>% rename(area_name = areaName) %>% 
-  rename(cases = cumCasesBySpecimenDate) %>% select(area_name, date, cases) 
-covid.london_england = mutate(covid.london_england, month = substring(date,1,7)) %>% select(-date)
-covid.london_england = covid.london_england %>% group_by(area_name, month) %>%
-  summarize_each(funs(max))
-
-covid.comparison = covid.london_england
-covid.comparison$area_name <- as.character(covid.comparison$area_name)
-covid.comparison$area_name[covid.comparison$area_name != "London"] = "Rest of England"
-covid.comparison = covid.comparison %>% group_by(area_name, month) %>%
-  summarize_each(funs(sum)) %>% filter(month != "2020-01")
 
 
-covid.cases = read.csv(url("https://data.london.gov.uk/download/coronavirus--covid-19--cases/151e497c-a16e-414e-9e03-9e428f555ae9/phe_cases_london_boroughs.csv"))
-covid.cases = mutate(covid.cases, month = substring(date,1,7)) %>% select(-c("date", "new_cases"))
-covid.cases = covid.cases %>% group_by(month, area_name, area_code) %>% 
-  summarize_each(funs(max)) 
-
-
-ldn = readOGR("data/covid/london", layer = "london")
-#proj4string(ldn) = CRS("+init=epsg:27700")
-#ldn.wgs84 = spTransform(ldn, CRS("+init=epsg:4326"))
-tr1 = ldn@polygons[[33]]@Polygons[[1]]@coords 
-tr2 = ldn@polygons[[21]]@Polygons[[1]]@coords 
-new_c = rbind(tr2[1:23,],tr1[6:11,],tr1[1:3,],tr2[24:26,])
-ldn@polygons[[33]]@Polygons[[1]]@coords = new_c
-ldn@polygons[[21]]@Polygons[[1]]@coords = new_c
-
-borough_and_id = ldn@data %>% as_tibble() %>% mutate(id = as.character(row_number()-1)) %>%
-  select(c("id", "name")) %>% filter(name != "City of London") %>% rename(area_name = name)
-borough_and_id$area_name <- as.character(borough_and_id$area_name)
-borough_and_id$area_name[borough_and_id$area_name == "Hackney"] = "Hackney and City of London"
-
-ldn_f = fortify(ldn) %>% filter (id != 32)
-
-
-covid.cases = left_join(covid.cases, borough_and_id, by = "area_name")
-
-ldn_f = left_join(ldn_f, covid.cases, by="id")
-
-
-
-england = readOGR("data/covid/england", layer = "eng")
-england_f = fortify(england)
-
-region_and_id = england@data %>% as_tibble() %>% mutate(id = as.character(row_number()-1)) %>%
-  select(c("id", "RGN20NM")) %>% rename(area_name = RGN20NM)
-
-covid.eng = covid.london_england
-covid.eng = left_join(covid.eng, region_and_id, by = "area_name")
-england_f = left_join(england_f, covid.eng, by="id") %>% rename(total_cases = cases)
-
-months = unique(ldn_f$month)[unique(ldn_f$month) %in% unique(england_f$month)]
-months = months[months %in% unique(covid.comparison$month)]
-
-months
-
-down_ldn = min(ldn_f$total_cases)
-up_ldn = max(ldn_f$total_cases)
-down_eng = 0
-up_eng = max(england_f$total_cases)
-
-ldn_f = ldn_f  %>% spread(month,total_cases)
-england_f = england_f %>% spread(month, total_cases)
-covid.comparison = covid.comparison %>% spread(month, cases)
-
-ldn_f[is.na(ldn_f)] = 0
-england_f[is.na(england_f)] = 0
-covid.comparison[is.na(covid.comparison)] = 0
-
-england_f = england_f %>% filter(area_name != "London")
-
-covid.deaths = read.csv(url("https://data.london.gov.uk/download/coronavirus--covid-19--deaths/aa17aaa1-2b9e-4e60-a0ee-5d8bbac31486/nhse_total_deaths_by_region.csv"))
-covid.deaths = mutate(covid.deaths, month = substring(date,1,7))
-covid.deaths = covid.deaths %>% rename( area_name = nhs_england_region) %>%
-  rename(`total deaths` = cumulative_deaths_total) %>% rename(`deaths with positive test` = cumulative_deaths_with_positive_test) %>%
-  rename(`deaths without positive test` = cumulative_deaths_without_positive_test)
-covid.deaths = covid.deaths %>% select(c("area_name","month", "total deaths", "deaths with positive test","deaths without positive test"))
-covid.deaths = covid.deaths %>% group_by(area_name, month) %>%
-  summarize_each(funs(max))
-covid.deaths$area_name[covid.deaths$area_name != "London"] = "Rest of England"
-covid.deaths = covid.deaths %>% group_by(area_name, month) %>%
-  summarize_each(funs(sum))
-covid.deaths_london = covid.deaths %>% filter(area_name == "London") %>% ungroup() %>% select(-area_name)
-covid.deaths_rest = covid.deaths %>% filter(area_name =="Rest of England") %>% ungroup() %>% select(-area_name)
-data.deaths_london = covid.deaths_london %>% select(c("month", "total deaths"))
-data.deaths_rest = covid.deaths_rest %>% select(c("month", "total deaths"))
-
-carto_pal(name="Emrld")
-carto_pal(name = "BurgYl")
-
-# ----------------- restrictions timetable formatting ------------------------------------------------------------------------
-
-restrictionsTable = read.csv(url("https://data.london.gov.uk/download/covid-19-restrictions-timeseries/03073b4a-2f5d-4a0a-be90-4fe3d9f609c9/restrictions_summary.csv"))
-restrictionsTable <- restrictionsTable %>% 
-  mutate(across("schools_closed", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
-  mutate(across("pubs_closed", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
-  mutate(across("shops_closed", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
-  mutate(across("eating_places_closed", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
-  mutate(across("stay_at_home", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
-  mutate(across("household_mixing_indoors_banned", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
-  mutate(across("wfh", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
-  mutate(across("rule_of_6_indoors", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
-  mutate(across("curfew", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
-  mutate(across("eat_out_to_help_out", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>% 
-  rename(
-    "schools closed" = schools_closed,
-    "pubs closed" = pubs_closed,
-    "shops closed" = shops_closed,
-    "eating_places_closed" = eating_places_closed,
-    "stay at home" = stay_at_home,
-    "household mixing indoors banned" = household_mixing_indoors_banned,
-    "work from home" = wfh,
-    "rule o 6 indoors" = rule_of_6_indoors, 
-    "eat out to help out" = eat_out_to_help_out
-  )
-
-# ----------------- activity processing -------------------------------------------------------------------------------------------
-
-activity = read.csv(url("https://data.london.gov.uk/download/google-mobility-by-borough/26d5821b-fcb6-4aae-af73-ee0596942d16/google_activity_by_London_Borough.csv"))
-names(activity) <-
-  sub("_percent_change_from_baseline", "", names(activity)) # remove annoying postfix
-
-# transform data, so that is can be better facet_wraped; move columns describing industry branches into one attribute called "Description"
-# and its value in "value" column
-data_long <- activity %>%
-  gather(Description, value, retail_and_recreation:residential)
-
-restrictions = read.csv(url("https://data.london.gov.uk/download/covid-19-restrictions-timeseries/03073b4a-2f5d-4a0a-be90-4fe3d9f609c9/restrictions_summary.csv"))
-
-activity$date <- as.character(activity$date)
-restrictions$date <- as.character(restrictions$date)
-
-data_long2 <-
-  left_join(data_long, restrictions, by = c("date" = "date"))
-data_long2 <- data_long2 %>% 
-  fill(restriction:eat_out_to_help_out) 
-
-data_long2$restriction <- as.character(data_long2$restriction)
-data_long2$restriction <- replace_na(data_long2$restriction, "None")
-
-data_long2$source <- as.character(data_long2$source)
-data_long2$source <- replace_na(data_long2$source, "None")
-
-data_long2[, 8:17][is.na(data_long2[, 8:17])] <- 0
-
-data_long2 <- data_long2%>% 
-  mutate(restriction = fct_relevel(
-    restriction,
-    "None",
-    "Work from home advised",
-    "Pubs and hospitality close",
-    "Schools close",
-    "Lockdown 1",
-    "Stay alert",
-    "Schools reopen",
-    "Shops reopen",
-    "Hospitality reopens",
-    "Covid secure workplaces introduced",
-    "Eat out to help out",
-    "End of eat out to help out",
-    "Rule of 6",
-    "WFH encouraged again",
-    "Curfew",
-    "Tier 1",
-    "Tier 2",
-    "Lockdown 2",
-    "Tier 3",
-    "Tier 4",
-    "Lockdown 3",
-    "Roadmap out of lockdown: Step 1",
-    "Roadmap out of lockdown: Step 2",
-    "Roadmap out of lockdown: Step 3"
-  )) 
-
-# ------------------------- S - E - R - V - E - R ---------------------------------------------------------------------------------
 shinyServer(function(input, output) {
+  restrictionsTable = read.csv(url("https://data.london.gov.uk/download/covid-19-restrictions-timeseries/03073b4a-2f5d-4a0a-be90-4fe3d9f609c9/restrictions_summary.csv"))
+  # ----------------- restrictions timetable formatting ------------------------------------------------------------------------
+  restrictionsTable <- restrictionsTable %>% 
+    mutate(across("schools_closed", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
+    mutate(across("pubs_closed", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
+    mutate(across("shops_closed", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
+    mutate(across("eating_places_closed", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
+    mutate(across("stay_at_home", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
+    mutate(across("household_mixing_indoors_banned", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
+    mutate(across("wfh", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
+    mutate(across("rule_of_6_indoors", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>%
+    mutate(across("curfew", ~factor(., levels=c(0,1), labels=c("no", "yes")))) %>%
+    mutate(across("eat_out_to_help_out", ~factor(., levels=c(0,1), labels=c("no","yes")))) %>% 
+    rename(
+      "schools closed" = schools_closed,
+      "pubs closed" = pubs_closed,
+      "shops closed" = shops_closed,
+      "eating_places_closed" = eating_places_closed,
+      "stay at home" = stay_at_home,
+      "household mixing indoors banned" = household_mixing_indoors_banned,
+      "work from home" = wfh,
+      "rule o 6 indoors" = rule_of_6_indoors, 
+      "eat out to help out" = eat_out_to_help_out
+    )
+  # ----------------- activity processing -------------------------------------------------------------------------------------------
+  activity = read.csv(url("https://data.london.gov.uk/download/google-mobility-by-borough/26d5821b-fcb6-4aae-af73-ee0596942d16/google_activity_by_London_Borough.csv"))
+  names(activity) <-
+    sub("_percent_change_from_baseline", "", names(activity)) # remove annoying postfix
   
+  # transform data, so that is can be better facet_wraped; move columns describing industry branches into one attribute called "Description"
+  # and its value in "value" column
+  data_long <- activity %>%
+    gather(Description, value, retail_and_recreation:residential)
+  
+  restrictions = read.csv(url("https://data.london.gov.uk/download/covid-19-restrictions-timeseries/03073b4a-2f5d-4a0a-be90-4fe3d9f609c9/restrictions_summary.csv"))
+  
+  activity$date <- as.character(activity$date)
+  restrictions$date <- as.character(restrictions$date)
+  
+  data_long2 <-
+    left_join(data_long, restrictions, by = c("date" = "date"))
+  data_long2 <- data_long2 %>% 
+    fill(restriction:eat_out_to_help_out) 
+  
+  data_long2$restriction <- as.character(data_long2$restriction)
+  data_long2$restriction <- replace_na(data_long2$restriction, "None")
+  
+  data_long2$source <- as.character(data_long2$source)
+  data_long2$source <- replace_na(data_long2$source, "None")
+  
+  data_long2[, 8:17][is.na(data_long2[, 8:17])] <- 0
+  
+  data_long2 <- data_long2%>% 
+    mutate(restriction = fct_relevel(
+      restriction,
+      "None",
+      "Work from home advised",
+      "Pubs and hospitality close",
+      "Schools close",
+      "Lockdown 1",
+      "Stay alert",
+      "Schools reopen",
+      "Shops reopen",
+      "Hospitality reopens",
+      "Covid secure workplaces introduced",
+      "Eat out to help out",
+      "End of eat out to help out",
+      "Rule of 6",
+      "WFH encouraged again",
+      "Curfew",
+      "Tier 1",
+      "Tier 2",
+      "Lockdown 2",
+      "Tier 3",
+      "Tier 4",
+      "Lockdown 3",
+      "Roadmap out of lockdown: Step 1",
+      "Roadmap out of lockdown: Step 2",
+      "Roadmap out of lockdown: Step 3"
+    )) 
+  
+  # target <- reactive({
+  #   input$checkbox1
+  # })
+  # fig <- plot_ly(data = (data_long2 %>% filter(Description %in% target)), type = "box", x = ~restriction, y = ~value, color = ~Description)
+  
+  
+  # --------------------- output rendering -------------------------------------------------------------------------------------------
   output$funnyBoxPlots = renderPlotly(plot_ly(data = (data_long2 %>% filter(Description %in% input$checkbox1)), type = "box", x = ~restriction, y = ~value, color = ~Description))
   output$table = renderDataTable({restrictionsTable})
   
-<<<<<<< HEAD
-  output$pie <- renderPlotly({
-    g = plot_ly(covid.comparison, labels = ~area_name, 
-                values = ~.data[[input$month]], type = "pie",
-=======
   
   #-------------------- maps data -------------------------------------------------------------------------------------------------
   covid.london_england = read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=region&metric=cumCasesBySpecimenDate&format=csv"))
@@ -316,7 +212,6 @@ shinyServer(function(input, output) {
   output$pie <- renderPlotly({
     g = plot_ly(covid.comparison, source = "pie", labels = ~area_name, 
                 values = covid.comparison[[toString(input$month)]], type = "pie",
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
                 textposition = 'inside',
                 textinfo = 'label',
                 insidetextfont = list(color = '#FFFFFF'),
@@ -325,19 +220,12 @@ shinyServer(function(input, output) {
                 showlegend = F)
     g %>% event_register("plotly_click")
     
-<<<<<<< HEAD
-=======
     
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
   })
   
   output$plot <- renderUI({
     
-<<<<<<< HEAD
-    d = event_data("plotly_click")$pointNumber
-=======
     d = event_data("plotly_click", source = "pie")$pointNumber
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
     if(!is.null(d)){
       if(d == 0){
         output$map <- renderPlotly({
@@ -408,5 +296,3 @@ shinyServer(function(input, output) {
   })
   
 })
-
-
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinydashboard)
 library(dplyr)
 library(ggplot2)
 library(plotly)
@@ -6,20 +7,7 @@ library(tidyr)
 library(ggpubr)
 library(forcats)
 library(ggridges)
-<<<<<<< HEAD
-library(rgdal)
-library(sp)
-library(rgeos)
-library(shinydashboard)
-library(inlmisc)
 library(shinyWidgets)
-library(RColorBrewer)
-library(rcartocolor)
-library(reactable)
-library(htmltools)
-=======
-library(shinyWidgets)
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
 
 # https://fontawesome.com/icons?d=gallery&p=2&q=chart 
 # TODO: go here and find cool chart icons instead of "th" below
@@ -37,64 +25,40 @@ dashboardPage(
     )
   ),
   dashboardBody(
-  tabItems(
-    tabItem(tabName = "start",
-            h3("Add random text or something idk")),
-    tabItem(tabName = "1", 
-            h3("TODO: add title here"),
-            selectInput("checkbox1", "Description",
-                               choices = c("Residential" = "residential",
-                                 "Retail and recreation" = "retail_and_recreation",
-                                 "Gorcery and pharmacy" = "grocery_and_pharmacy",
-                                 "Parks" = "parks",
-                                 "Transit stations" = "transit_stations",
-                                 "Workplaces" = "workplaces"), selected = "residental",
-                        multiple = TRUE),
-            plotlyOutput("funnyBoxPlots"),
-            dataTableOutput("table")),
-    tabItem(tabName = "2",
-<<<<<<< HEAD
-            h3("plz work"),
-            fluidRow(
-              box(width = 12,
-                  sliderTextInput("month", 
-                                  "Select date:",
-                                  choices = c("2020-02" = "2020-02", "2020-03" = "2020-03",  
-                                              "2020-04" = "2020-04", "2020-05" = "2020-05",
-                                              "2020-06" = "2020-06", "2020-07" = "2020-07",
-                                              "2020-08" = "2020-08", "2020-09" = "2020-09",
-                                              "2020-10" = "2020-10", "2020-11" = "2020-11",
-                                              "2020-12" = "2020-12", "2021-01" = "2021-01",
-                                              "2021-02" = "2021-02", "2021-03" = "2021-03", 
-                                              "2021-04" = "2021-04", "2021-05" = "2021-05"),
-                                  selected = "2020-02",
-                                  # choices = months,
-                                  # selected = months[1],
-                                  animate = T),
-=======
-            h3("Title"),
-            fluidRow(
-              box(width = 12,
-                  uiOutput("slider"),
->>>>>>> c6f6a449803926cc495f65705422906c31bb9a0c
-                  plotlyOutput("pie")
-              )
-            ),
-            fluidRow(
-              uiOutput("plot")
-            )),
-    tabItem(tabName = "3",
-            h3("Add visualization")),
-    tabItem(tabName = "4",
-            h3("Add visualization")),
-    tabItem(tabName = "5",
-            h3("Add visualization")),
-    tabItem(tabName = "6",
-            h3("Add visualization"))
+    tabItems(
+      tabItem(tabName = "start",
+              h3("Add random text or something idk")),
+      tabItem(tabName = "1", 
+              h3("TODO: add title here"),
+              selectInput("checkbox1", "Description",
+                          choices = c("Residential" = "residential",
+                                      "Retail and recreation" = "retail_and_recreation",
+                                      "Gorcery and pharmacy" = "grocery_and_pharmacy",
+                                      "Parks" = "parks",
+                                      "Transit stations" = "transit_stations",
+                                      "Workplaces" = "workplaces"), selected = "residental",
+                          multiple = TRUE),
+              plotlyOutput("funnyBoxPlots"),
+              dataTableOutput("table")),
+      tabItem(tabName = "2",
+              h3("Title"),
+              fluidRow(
+                box(width = 12,
+                    uiOutput("slider"),
+                    plotlyOutput("pie")
+                )
+              ),
+              fluidRow(
+                uiOutput("plot")
+              )),
+      tabItem(tabName = "3",
+              h3("Add visualization")),
+      tabItem(tabName = "4",
+              h3("Add visualization")),
+      tabItem(tabName = "5",
+              h3("Add visualization")),
+      tabItem(tabName = "6",
+              h3("Add visualization"))
     )
   )
 )
-
-
-
-
